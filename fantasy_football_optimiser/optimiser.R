d6e7dc05a57763700c3b74504b510c5b630a5c4a
Martin Eastwood <martin.eastwood1@gmail.com>
2016-10-11 20:33:46 +0100

    updated fantasy football optimiser

--- fantasy_football_optimiser/optimiser.R
+++ fantasy_football_optimiser/optimiser.R
@@ -1,23 +1,15 @@
 library(lpSolve)
-library(stringr)
 library(RCurl)
+library(stringr)
 library(jsonlite)
-library(plyr)
+library(dplyr)
 
-# scrape the data
-df <- ldply(1:612, function(x){
-  # Scrape responsibly kids, we don't want to ddos
-  # the Fantasy Premier League's website
-  Sys.sleep(0.05)
-  print(sprintf('Scraping player %s', x))
-  url <- sprintf("http://fantasy.premierleague.com/web/api/elements/%s/?format=json", x)
-  json <- fromJSON(getURL(url))
-  json$now_cost <- json$now_cost / 10
-  data.frame(json[names(json) %in% c('web_name', 'team_name', 'type_name', 
-                                     'now_cost', 'total_points')])
-})
-# Tidy it up as Evra still in there
-#df <- subset(df, !web_name %in% c('Evra'))
+df <- "https://fantasy.premierleague.com/drf/bootstrap-static" %>% 
+  getURL %>% 
+  fromJSON %>% 
+  .$elements %>% 
+  data.frame %>% 
+  mutate(now_cost = now_cost / 10)
 
 # The vector to optimize on
 objective <- df$total_points
@@ -27,17 +19,17 @@ num_gk <- 2
 num_def <- 5
 num_mid <- 5
 num_fwd <- 3
-max_cost <- 100.7
+max_cost <- 100
 
 # Create vectors to constrain by position
-df$Goalkeeper <- ifelse(df$type_name == "Goalkeeper", 1, 0)
-df$Defender <- ifelse(df$type_name == "Defender", 1, 0)
-df$Midfielder <- ifelse(df$type_name == "Midfielder", 1, 0)
-df$Forward <- ifelse(df$type_name == "Forward", 1, 0)
+df$Goalkeeper <- ifelse(df$element_type == "1", 1, 0)
+df$Defender <- ifelse(df$element_type == "2", 1, 0)
+df$Midfielder <- ifelse(df$element_type == "3", 1, 0)
+df$Forward <- ifelse(df$element_type == "4", 1, 0)
 
 # Create constraint vectors to constrain by max number of players allowed per team
-team_constraint <- unlist(lapply(unique(df$team_name), function(x, df){
-  ifelse(df$team_name==x, 1, 0)
+team_constraint <- unlist(lapply(unique(df$team_code), function(x, df){
+  ifelse(df$team_code==x, 1, 0)
 }, df=df))
 
 # next we need the constraint directions
@@ -46,15 +38,21 @@ const_dir <- c("=", "=", "=", "=", rep("<=", 21))
 # Now put the complete matrix together
 const_mat <- matrix(c(df$Goalkeeper, df$Defender, df$Midfielder, df$Forward, 
                       df$now_cost, team_constraint), 
-                    nrow=(5 + length(unique(df$team_name))), byrow=TRUE)
+                    nrow=(5 + length(unique(df$team_code))), byrow=TRUE)
 const_rhs <- c(num_gk, num_def, num_mid, num_fwd, max_cost, rep(3, 20))
 
 # then solve the matrix
 x <- lp ("max", objective, const_mat, const_dir, const_rhs, all.bin=TRUE, all.int=TRUE)
 
 # And this is our team!
-print(arrange(df[which(x$solution==1),], desc(Goalkeeper), desc(Defender), 
-              desc(Midfielder), desc(Forward), desc(total_points)))
-print(str_c('Total Price: ', sum(df[which(x$solution==1), 'now_cost'])))
-print(str_c('Total Points: ', sum(df[which(x$solution==1), 'total_points'])))
+solution <- df %>% 
+  mutate(solution = x$solution) %>% 
+  filter(solution == 1) %>% 
+  select(web_name, element_type, now_cost, total_points) %>% 
+  arrange(element_type) 
+
+print(solution)
+
+solution %>% summarise(total_price = sum(now_cost)) %>% print
+solution %>% summarise(total_proints = sum(total_points)) %>% print
 
